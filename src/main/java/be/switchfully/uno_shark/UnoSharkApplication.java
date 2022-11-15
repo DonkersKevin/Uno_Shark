abd70b1e92613b8d3c1bba66c303ad336f6520e6
DonkersKevin <donkers.kevin86@gmail.com>
2022-11-15 13:24:49 +0100

    First commit

---
+++ src/main/java/be/switchfully/uno_shark/UnoSharkApplication.java
@@ -0,0 +1,13 @@
+package be.switchfully.uno_shark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnoSharkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnoSharkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/switchfully/uno_shark/UnoSharkApplicationTests.java
@@ -0,0 +1,13 @@
+package be.switchfully.uno_shark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnoSharkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
