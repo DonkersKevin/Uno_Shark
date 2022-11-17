3f577736fed8da5fd997944a0d2a94426a62f638
St12n <stijn.schouppe@hotmail.com>
2022-11-17 15:06:51 +0100

    Address & PostalCode - tests for edge cases done --> 100 %

--- src/test/java/be/switchfully/uno_shark/domain/person/address/PostalCodeTest.java
+++ src/test/java/be/switchfully/uno_shark/domain/person/address/PostalCodeTest.java
@@ -1,7 +1,43 @@
 package be.switchfully.uno_shark.domain.person.address;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 
 class PostalCodeTest {
 
+    @Test
+    void givenValidPostalCode_NoExceptionsThrown() {
+        PostalCode postalCode = new PostalCode("1000", "BXL");
+        assertThat(postalCode).isNotNull();
+    }
+
+    @Test
+    void givenNoPostalCode_getException() {
+        assertThatThrownBy(() -> new PostalCode(null, "BXL"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Postal Code cannot be empty.");
+    }
+
+    @Test
+    void givenTooLongPostalCode_getException() {
+        assertThatThrownBy(() -> new PostalCode("99999", "BXL"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Provide a valid postal code: format ####");
+    }
+
+    @Test
+    void givenNoCityName_getException() {
+        assertThatThrownBy(() -> new PostalCode("9999", null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Provide a city name.");
+    }
+
+    @Test
+    void givenTwoCitiesSameInfo_AreEqual() {
+        PostalCode postalCode1 = new PostalCode("1000", "Brussel");
+        PostalCode postalCode2 = new PostalCode("1000", "Brussel");
+        assertThat(postalCode1).isEqualTo(postalCode2);
+        assertThat(postalCode1.hashCode()).isEqualTo(postalCode2.hashCode());
+    }
 }
