2f8437f6c9b648022192a036ecc67bfb7fa7c838
St12n <stijn.schouppe@hotmail.com>
2022-11-17 10:33:00 +0100

    ParkingLotTest added - all null tests for parkinglot done.

--- src/test/java/be/switchfully/uno_shark/domain/parking/ParkinglotTest.java
+++ src/test/java/be/switchfully/uno_shark/domain/parking/ParkinglotTest.java
@@ -1,5 +1,7 @@
 package be.switchfully.uno_shark.domain.parking;
 
+import be.switchfully.uno_shark.domain.person.Person;
+import be.switchfully.uno_shark.domain.person.address.Address;
 import org.junit.jupiter.api.Test;
 
 
@@ -29,5 +31,43 @@ class ParkinglotTest {
         assertThatThrownBy(() -> new Parkinglot("De Brouckere", ParkingCategory.UNDERGROUND, null, 100, null, null, null))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("Provide a valid Division for this parking lot.");
+    }
+
+    @Test
+    void ifCapacityIsLowerThanZero_ThrowIllegalArgument() {
+        //GIVEN
+        assertThatThrownBy(() -> new Parkinglot("De Brouckere", ParkingCategory.UNDERGROUND, new Division(), -100, null, null, null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Provide a valid capacity for this parking lot.");
+    }
+
+    @Test
+    void ifPersonIsNull_ThrowIllegalArgument() {
+        //GIVEN
+        assertThatThrownBy(() -> new Parkinglot("De Brouckere", ParkingCategory.UNDERGROUND, new Division(), 100, null, null, null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Provide a valid Contactperson for this parking lot.");
+    }
+
+    @Test
+    void ifAddressIsNull_ThrowIllegalArgument() {
+        //GIVEN
+        assertThatThrownBy(() -> new Parkinglot("De Brouckere", ParkingCategory.UNDERGROUND, new Division(), 100, new Person(), null, null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Provide a valid address for this parking lot.");
+    }
+
+    @Test
+    void ifPricePerHourIsNull_ThrowIllegalArgument() {
+        //GIVEN
+        assertThatThrownBy(() -> new Parkinglot("De Brouckere", ParkingCategory.UNDERGROUND, new Division(), 100, new Person(), new Address(), null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Provide a valid price per hour.");
+    }
+
+    @Test
+    void ifGivenCorrectParkingLot_thenAllOke() {
+        Parkinglot parkinglot = new Parkinglot("De Brouckere", ParkingCategory.UNDERGROUND, new Division(), 100, new Person(), new Address(), new Price());
+        assertThat(parkinglot).isNotNull();
     }
 }
