6640ad736573d10604b9d7ee702b162301260ced
Ruben Fobe <ruben.fobe@mobilit.fgov.be>
2022-11-18 15:07:48 +0100

    test show allocations

---
+++ src/test/java/be/switchfully/uno_shark/controllers/ParkingSpotAllocationControllerTest.java
@@ -0,0 +1,120 @@
+package be.switchfully.uno_shark.controllers;
+
+import be.switchfully.uno_shark.domain.parking.divisionDto.ShowDivisionDto;
+import be.switchfully.uno_shark.domain.parkingspotallocation.dto.ShowAllocationDto;
+import be.switchfully.uno_shark.repositories.SpotAllocationRepository;
+import be.switchfully.uno_shark.services.SpotAllocationService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestDatabase
+class ParkingSpotAllocationControllerTest {
+
+    @Autowired
+    private SpotAllocationRepository spotRepo;
+
+    @Autowired
+    private SpotAllocationService spotService;
+
+    @LocalServerPort
+    private int port;
+
+    @Test
+    void findAllWithoutFilterHappyPath(){
+        ShowAllocationDto[] response = given().baseUri("http://localhost")
+                .port(port)
+                .when()
+                .get("/spotallocation")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.OK.value())
+                .extract()
+                .as(ShowAllocationDto[].class);
+
+        assertEquals(response.length, spotRepo.findAll().size());
+
+    }
+
+    @Test
+    void findAllWithLimitHappyPath(){
+        ShowAllocationDto[] response = given().baseUri("http://localhost")
+                .port(port)
+                .when()
+                .get("/spotallocation?limit=2")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.OK.value())
+                .extract()
+                .as(ShowAllocationDto[].class);
+
+        assertEquals(response.length, 2);
+
+    }
+
+    @Test
+    void findAllDescendingHappyPath(){
+        ShowAllocationDto[] response = given().baseUri("http://localhost")
+                .port(port)
+                .when()
+                .get("/spotallocation?sort=descending")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.OK.value())
+                .extract()
+                .as(ShowAllocationDto[].class);
+
+        assertThat(response).isSortedAccordingTo(Comparator.reverseOrder());
+
+    }
+
+    @Test
+    void findAllStatusActiveHappyPath(){
+        ShowAllocationDto[] response = given().baseUri("http://localhost")
+                .port(port)
+                .when()
+                .get("/spotallocation?status=active")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.OK.value())
+                .extract()
+                .as(ShowAllocationDto[].class);
+
+        assertThat(Arrays.stream(response)
+                .filter(showAllocationDto -> showAllocationDto.getStopTime() != null).toList()).isEmpty();
+
+    }
+
+    @Test
+    void findAllStatusStoppedHappyPath(){
+        ShowAllocationDto[] response = given().baseUri("http://localhost")
+                .port(port)
+                .when()
+                .get("/spotallocation?status=stopped")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.OK.value())
+                .extract()
+                .as(ShowAllocationDto[].class);
+
+        assertThat(Arrays.stream(response)
+                .filter(showAllocationDto -> showAllocationDto.getStopTime() == null).toList()).isEmpty();
+
+    }
+
+
+
+}
